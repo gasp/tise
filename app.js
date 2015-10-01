d497333ddca4546e1d77cd3d80420fad5e529761
gaspard <gaspard@gmail.com>
2015-10-01 12:49:22 +0200

    backoffice on /admin

--- app.js
+++ app.js
@@ -9,9 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/admin', function (req, res) {
+  res.sendFile(__dirname + '/admin.html');
+});
+
 io.on('connection', function (socket) {
   console.log('a user connected');
   socket.emit('message', 'welcome');
+  socket.on('drink', function (what) {
+    console.log('just drinked a ', what);
+  });
 });
 
 http.listen(3000, function () {
