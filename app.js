6aa7017dc42fbae18c326c6e57c32a31c2d60e3f
gaspard <gaspard@gmail.com>
2015-10-01 12:40:33 +0200

    sent a welcome message through socket.emit

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
   console.log('a user connected');
+  socket.emit('message', 'welcome');
 });
 
 http.listen(3000, function () {
