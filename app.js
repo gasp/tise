fc935a7ee5458e1ab78b826c3ec95aa99c1188e3
gaspard <gaspard@gmail.com>
2015-10-01 12:30:48 +0200

    Yay, first socket.io connection !

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-
 app.get('/', function (req, res) {
-  res.end('bienvenue sur index');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  console.log('a user connected');
 });
 
 http.listen(3000, function () {
